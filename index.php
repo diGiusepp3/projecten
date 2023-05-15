230ef744a064da76a4c18d9effeddcc5acf21c43
Matthias Gielen <107040873+diGiusepp3@users.noreply.github.com>
2023-05-15 09:31:57 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+include('../../header.php');
+echo"
+<div class='projecten container'>
+    <h1>Projecten</h1>
+    <div class='project'>
+        <h2>Rekenmachine</h2>
+        
+    </div>
+    <div class='project'>
+        <h2>Quiz App</h2>
+    </div>
+    <div class='project'>
+        <h2>Blad steen schaar</h2>
+    </div>
+</div>
+";
+
+include('../../footer.php');
